b6ffe7e2f96a659cb17a6bf084f5b763f47d4af8
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-13 08:09:44 -0300

    Criando array a partr do Objeto

---
+++ src/br/com/senai/ArrayReferenciasTest.java
@@ -0,0 +1,21 @@
+package br.com.senai;
+
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class ArrayReferenciasTest {
+    public static void main(String[] args) {
+        ContaCorrente[] contas = new ContaCorrente[5];
+        ContaCorrente cc1 = new ContaCorrente(123, 456);
+        contas[0] = cc1;
+
+        ContaCorrente cc2 = new ContaCorrente(789, 852);
+        contas[1] = cc2;
+
+        System.out.println(contas[1].getNumero());
+        ContaCorrente ref = contas[1];
+
+        System.out.println(ref.getNumero());
+        System.out.println(cc2.getNumero());
+
+    }
+}
