c2443a94eddc670df9634e67c6d612713282ae33
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-13 10:05:06 -0300

    Criando um array da classe Object e fazendo um cast para um Objeto mais específico

---
+++ src/br/com/senai/ArrayReferenciaTest2.java
@@ -0,0 +1,30 @@
+package br.com.senai;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class ArrayReferenciaTest2 {
+    public static void main(String[] args) {
+        Conta[] contas = new Conta[5];
+        ContaPoupanca cp = new ContaPoupanca(123, 456);
+        ContaCorrente cc = new ContaCorrente(456, 123);
+
+        contas[1] = cc;
+        System.out.println(cc.getNumero());
+
+        contas[0] = cp;
+        System.out.println(cp.getNumero());
+
+        Conta ref = contas[0];
+        System.out.println(ref.getNumero());
+
+        System.out.println("################################");
+
+        Object[] referencia = new Object[5];
+        referencia[0] = cc;
+        Object conta = (ContaCorrente) referencia[0];
+        System.out.println(((ContaCorrente) referencia[0]).getNumero());
+
+    }
+}
