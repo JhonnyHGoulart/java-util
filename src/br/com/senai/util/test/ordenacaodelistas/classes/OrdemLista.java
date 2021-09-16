b57648e6d2758cb7406735165a5e29e87ef9c7de
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-16 07:55:04 -0300

    Otimizando o código da classe OrdemLista

--- src/br/com/senai/util/test/ordenacaodelistas/classes/OrdemLista.java
+++ src/br/com/senai/util/test/ordenacaodelistas/classes/OrdemLista.java
@@ -8,12 +8,16 @@ public class OrdemLista implements Comparator<Conta> {
 
     @Override
     public int compare(Conta c1, Conta c2) {
-        if (c1.getNumero() < c2.getNumero()) {
-            return -9862;
-        }
-        if (c1.getNumero() > c2.getNumero()) {
-            return 1;
-        }
-        return 0;
+       return Integer.compare(c1.getNumero(),c2.getNumero());
+
+//        return c1.getNumero() - c2.getNumero();
+
+//        if (c1.getNumero() < c2.getNumero()) {
+//            return -9862;
+//        }
+//        if (c1.getNumero() > c2.getNumero()) {
+//            return 1;
+//        }
+//        return 0;
     }
 }
